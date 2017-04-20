44a59f02d5f38d9155e8835a7e60072694c1f6f7
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-04-20 20:13:54 +0200

    Animate and get backers and supporters
    This will load all backers and sponsor images, and fade them in - untill
    they are loaded a loading animation will play.
    
    Polyfills for fetch and promises included.
    
    ES5ify code.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
-Promise.all(['https://opencollective.com/gulpjs/sponsor.json','https://opencollective.com/gulpjs/backer.json'].map(url =>
-  fetch(url).then(resp => resp.json())
-)).then(entries => {
-  let html = '';
-	const supporters = [].concat.apply([], entries);
-	const supportersToDisplay = supporters
-    .sort( (first, second) => {return (second.totalDonations - first.totalDonations)} )
-    .slice(0, 10);
+(function(){
+  var services = [
+    'https://opencollective.com/gulpjs/sponsor.json',
+    'https://opencollective.com/gulpjs/backer.json'
+  ].map(function (url) {
+    return fetch(url)
+      .then(function(resp) {
+        return resp.json();
+      });
+  })
 
-	supportersToDisplay.forEach((supporter) => {
-    html += `<a class="supporter" href="${supporter.website}"><img alt="${supporter.name}" src="${supporter.avatar}" width="112"></a>`
-	});
+  Promise.all(services).then(function (entries) {
+    var fragment = document.createDocumentFragment();
+    var nodes = [];
+  	var supporters = [].concat.apply([], entries);
+  	var supportersToDisplay = supporters
+      .sort(function(first, second) {
+        return (second.totalDonations - first.totalDonations)
+      })
+      .slice(0, 10);
 
-  document.getElementById('supporters').innerHTML = html;
-});
+  	supportersToDisplay.forEach(function(supporter) {
+      var img = new Image();
+      img.src = 'https://opencollective.com/proxy/images/?src=' + supporter.avatar ;
+      img.width = 80;
+      img.onload = function(e) {
+        e.currentTarget.parentNode.classList.remove('supporter--skeleton');
+      }
+
+      var link = document.createElement('a');
+      link.className = 'supporter supporter--skeleton';
+      link.href = supporter.website;
+
+      link.appendChild(img);
+      nodes.push(link);
+  	});
+
+    nodes.forEach(function(node) {
+      fragment.appendChild(node);
+    });
+
+    document.getElementById('supporters').innerHTML = ''; // Clear prerendered items
+    document.getElementById('supporters').appendChild( fragment );
+  });
+})();
