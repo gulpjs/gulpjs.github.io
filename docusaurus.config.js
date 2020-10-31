a75b9d0b886491ea9da9bde0d870923d6f089df0
Donghwi Park <donghwipark@gmail.com>
2020-10-31 10:44:59 +0900

    fix double quoute to single

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,14 +3,14 @@
 var isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  title: "gulp.js",
-  favicon: "img/favicon.png",
-  url: "https://gulpjs.com/",
-  baseUrl: "/",
-  tagline: "A toolkit to automate & enhance your workflow",
+  title: 'gulp.js',
+  favicon: 'img/favicon.png',
+  url: 'https://gulpjs.com/',
+  baseUrl: '/',
+  tagline: 'A toolkit to automate & enhance your workflow',
   // Used by the deployment
-  organizationName: "gulpjs",
-  projectName: "gulpjs.github.io",
+  organizationName: 'gulpjs',
+  projectName: 'gulpjs.github.io',
   // The theme
   themeConfig: {
     colorMode: {
@@ -19,53 +19,53 @@ module.exports = {
     },
     navbar: {
       logo: {
-        alt: "gulp",
-        src: "img/gulp.svg",
-        href: "/",
-        target: "_self",
+        alt: 'gulp',
+        src: 'img/gulp.svg',
+        href: '/',
+        target: '_self',
       },
       items: [
         {
-          to: "docs/en/getting-started/quick-start",
-          label: "Get Started",
-          position: "left",
+          to: 'docs/en/getting-started/quick-start',
+          label: 'Get Started',
+          position: 'left',
         },
         {
-          to: "docs/en/api/concepts",
-          label: "API",
-          position: "left",
+          to: 'docs/en/api/concepts',
+          label: 'API',
+          position: 'left',
         },
         {
-          to: "plugins",
-          label: "Plugins",
-          position: "left",
+          to: 'plugins',
+          label: 'Plugins',
+          position: 'left',
         },
         {
-          href: "https://github.com/sponsors/gulpjs",
-          label: "Donate",
-          position: "left",
+          href: 'https://github.com/sponsors/gulpjs',
+          label: 'Donate',
+          position: 'left',
         },
         {
-          to: "docs/en/support/for-enterprise",
-          label: "Enterprise",
-          position: "left",
+          to: 'docs/en/support/for-enterprise',
+          label: 'Enterprise',
+          position: 'left',
           emphasis: true,
         },
         {
-          href: "https://twitter.com/gulpjs",
+          href: 'https://twitter.com/gulpjs',
           logo: {
-            alt: "Gulp on Twitter",
-            src: "img/twitter.svg",
+            alt: 'Gulp on Twitter',
+            src: 'img/twitter.svg',
           },
-          position: "right",
+          position: 'right',
         },
         {
-          href: "https://medium.com/gulpjs",
+          href: 'https://medium.com/gulpjs',
           logo: {
-            alt: "The gulp blog",
-            src: "img/medium.svg",
+            alt: 'The gulp blog',
+            src: 'img/medium.svg',
           },
-          position: "right",
+          position: 'right',
         },
       ],
     },
@@ -81,32 +81,32 @@ module.exports = {
           ],
         },
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              to: "docs/en/getting-started/quick-start",
-              label: "Getting Started",
+              to: 'docs/en/getting-started/quick-start',
+              label: 'Getting Started',
             },
             {
-              to: "docs/en/api/concepts",
-              label: "API",
+              to: 'docs/en/api/concepts',
+              label: 'API',
             },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
             {
-              href: "https://github.com/gulpjs/gulp",
-              label: "GitHub",
+              href: 'https://github.com/gulpjs/gulp',
+              label: 'GitHub',
             },
             {
-              href: "https://stackoverflow.com/questions/tagged/gulp",
-              label: "Stack Overflow",
+              href: 'https://stackoverflow.com/questions/tagged/gulp',
+              label: 'Stack Overflow',
             },
             {
-              href: "https://twitter.com/gulpjs",
-              label: "Twitter",
+              href: 'https://twitter.com/gulpjs',
+              label: 'Twitter',
             },
           ],
         },
@@ -116,103 +116,103 @@ module.exports = {
       // One of:
       // dracula, duotoneDark, duotoneLight, github, nightOwl, nightOwlLight,
       // oceanicNext, palenight, shad esOfPurple, ultramin, vsDark
-      theme: require("prism-react-renderer/themes/vsDark"),
+      theme: require('prism-react-renderer/themes/vsDark'),
     },
     algolia: {
-      apiKey: "a6ef919bce0b83de1bcbad1d4ef753f8",
-      indexName: "gulpjs",
+      apiKey: 'a6ef919bce0b83de1bcbad1d4ef753f8',
+      indexName: 'gulpjs',
       algoliaOptions: {}, // Optional, if provided by Algolia
     },
     googleAnalytics: {
-      trackingID: "UA-128126650-1",
+      trackingID: 'UA-128126650-1',
     },
     gtag: {
-      trackingID: "UA-128126650-1",
+      trackingID: 'UA-128126650-1',
     },
     companyLogos: [
       {
-        href: "https://stickermule.com",
-        src: "sponsor-logos/sticker-mule.svg",
-        alt: "Sticker Mule logo",
-        title: "Sticker Mule",
+        href: 'https://stickermule.com',
+        src: 'sponsor-logos/sticker-mule.svg',
+        alt: 'Sticker Mule logo',
+        title: 'Sticker Mule',
         style: {
-          flexBasis: "120px",
+          flexBasis: '120px',
         },
       },
       {
-        href: "https://icons8.com/",
-        src: "sponsor-logos/icons8.svg",
-        alt: "Icons8 logo",
-        title: "Icons8",
+        href: 'https://icons8.com/',
+        src: 'sponsor-logos/icons8.svg',
+        alt: 'Icons8 logo',
+        title: 'Icons8',
         style: {
-          flexBasis: "80px",
+          flexBasis: '80px',
         },
       },
       {
-        href: "https://frontendmasters.com/",
-        src: "sponsor-logos/frontend-masters.png",
-        alt: "Frontend Masters logo",
-        title: "Frontend Masters",
+        href: 'https://frontendmasters.com/',
+        src: 'sponsor-logos/frontend-masters.png',
+        alt: 'Frontend Masters logo',
+        title: 'Frontend Masters',
         style: {
-          flexBasis: "200px",
+          flexBasis: '200px',
         },
       },
       {
-        href: "https://www.codeinwp.com/",
-        src: "sponsor-logos/codeinwp.svg",
-        alt: "CodeinWP",
-        title: "CodeinWP",
+        href: 'https://www.codeinwp.com/',
+        src: 'sponsor-logos/codeinwp.svg',
+        alt: 'CodeinWP',
+        title: 'CodeinWP',
         style: {
-          flexBasis: "150px",
+          flexBasis: '150px',
         },
       },
       {
-        href: "https://clay.global/",
-        src: "sponsor-logos/clay.png",
-        alt: "Clay logo",
-        title: "Clay",
+        href: 'https://clay.global/',
+        src: 'sponsor-logos/clay.png',
+        alt: 'Clay logo',
+        title: 'Clay',
         style: {
-          flexBasis: "150px",
+          flexBasis: '150px',
         },
       },
       {
-        href: "https://twitter.com/chromiumdev",
-        src: "sponsor-logos/chrome.svg",
-        alt: "Chrome logo",
-        title: "Chrome Developers",
+        href: 'https://twitter.com/chromiumdev',
+        src: 'sponsor-logos/chrome.svg',
+        alt: 'Chrome logo',
+        title: 'Chrome Developers',
         style: {
-          flexBasis: "80px",
+          flexBasis: '80px',
         },
       },
     ],
   },
   stylesheets: [
-    "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&family=Roboto:wght@900&display=swap",
+    'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400&family=Roboto:wght@900&display=swap',
   ],
   themes: [
     [
-      "@docusaurus/theme-classic",
+      '@docusaurus/theme-classic',
       {
-        customCss: require.resolve("./src/css/docs.css"),
+        customCss: require.resolve('./src/css/docs.css'),
       },
     ],
-    ["@docusaurus/theme-search-algolia", {}],
+    ['@docusaurus/theme-search-algolia', {}],
   ],
   plugins: [
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        path: "docs",
-        sidebarPath: require.resolve("./sidebars.json"),
+        path: 'docs',
+        sidebarPath: require.resolve('./sidebars.json'),
         // This is a holdover because we set up original docusaurus
         // to support translations and v2 doesn't support them yet
-        routeBasePath: "docs/en/",
+        routeBasePath: 'docs/en/',
       },
     ],
-    "docusaurus-plugin-sass",
-    ["@docusaurus/plugin-content-pages", {}],
-    ["@docusaurus/plugin-google-analytics", {}],
-    ["@docusaurus/plugin-google-gtag", {}],
-    ["@docusaurus/plugin-sitemap", {}],
+    'docusaurus-plugin-sass',
+    ['@docusaurus/plugin-content-pages', {}],
+    ['@docusaurus/plugin-google-analytics', {}],
+    ['@docusaurus/plugin-google-gtag', {}],
+    ['@docusaurus/plugin-sitemap', {}],
   ],
 };
